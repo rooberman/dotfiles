d44308347a954ba8737d7bd89dacfa34091ace90
rooberman <rooberman@gmail.com>
2020-10-09 16:30:45 +0200

    Añade scripts de docker

---
+++ terminal/_aliases/docker.sh
@@ -0,0 +1,9 @@
+#alias dc='dot docker connect'
+alias dl="docker_list"
+alias dcc="$DOTFILES_PATH/scripts/docker_connect"
+alias dcstop="$DOTFILES_PATH/scripts/docker_container_stop"
+alias dcs=dcstop
+alias dcstart="$DOTFILES_PATH/scripts/docker_container_start"
+alias dcr="$DOTFILES_PATH/scripts/docker_container_remove"
+
+alias di="docker images"

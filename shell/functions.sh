fb3958c8a3786fa08907b7bc441a43ced36809fd
Roberto Martínez Hernández <roberto.martinez@deiser.com>
2024-03-11 08:29:58 +0100

    Initial commit
    Incluye algunos alias personales y la función docker_list de prueba

---
+++ shell/functions.sh
@@ -0,0 +1,27 @@
+function cdd() {
+	cd "$(ls -d -- */ | fzf)" || echo "Invalid directory"
+}
+
+function j() {
+	fname=$(declare -f -F _z)
+
+	[ -n "$fname" ] || source "$DOTLY_PATH/modules/z/z.sh"
+
+	_z "$1"
+}
+
+function recent_dirs() {
+	# This script depends on pushd. It works better with autopush enabled in ZSH
+	escaped_home=$(echo $HOME | sed 's/\//\\\//g')
+	selected=$(dirs -p | sort -u | fzf)
+
+	cd "$(echo "$selected" | sed "s/\~/$escaped_home/")" || echo "Invalid directory"
+}
+
+# funcion creada en el curso de zsh
+function docker_list() {
+  containers=$(docker ps | awk '{if (NR!=1) print $1 ": " $(NF)}')
+
+  echo "Containers: 👇"
+  echo $containers
+}

4d786a4971d45f00f354e48fd952ed348f3a81a2
Roberto Martínez Hernández <roberto.martinez@deiser.com>
2024-04-06 14:41:03 +0200

    Disable some unused alias

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -11,19 +11,19 @@ alias dotfiles='cd $DOTFILES_PATH'
 # Git
 alias gaa="git add -A"
 alias gc='$DOTLY_PATH/bin/dot git commit'
-alias gca="git add --all && git commit --amend --no-edit"
-alias gco="git checkout"
+#alias gca="git add --all && git commit --amend --no-edit"
+#alias gco="git checkout"
 alias gd='$DOTLY_PATH/bin/dot git pretty-diff'
 alias gs="git status -sb"
 alias gf="git fetch --all -p"
 alias gps="git push"
-alias gpsf="git push --force"
-alias gpl="git pull --rebase --autostash"
-alias gb="git branch"
-alias gl='$DOTLY_PATH/bin/dot git pretty-log'
+#alias gpsf="git push --force"
+#alias gpl="git pull --rebase --autostash"
+#alias gb="git branch"
+#alias gl='$DOTLY_PATH/bin/dot git pretty-log'
 
 # Utils
-alias k='kill -9'
+#alias k='kill -9'
 alias i.='(idea $PWD &>/dev/null &)'
 alias c.='(code $PWD &>/dev/null &)'
 alias o.='open .'
@@ -31,11 +31,11 @@ alias up='dot package update_all'
 
 # Docker
 alias dl="docker_list"
-alias dcc="$DOTFILES_PATH/scripts/docker_connect"
-alias dcstop="$DOTFILES_PATH/scripts/docker_container_stop"
-alias dcs=dcstop
-alias dcstart="$DOTFILES_PATH/scripts/docker_container_start"
-alias dcr="$DOTFILES_PATH/scripts/docker_container_remove"
+#alias dcc="$DOTFILES_PATH/scripts/docker_connect"
+#alias dcstop="$DOTFILES_PATH/scripts/docker_container_stop"
+#alias dcs=dcstop
+#alias dcstart="$DOTFILES_PATH/scripts/docker_container_start"
+#alias dcr="$DOTFILES_PATH/scripts/docker_container_remove"
 alias di="docker images"
 
 # Personal
